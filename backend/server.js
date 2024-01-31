8b899f0f903e1b06d828ba21f3b686091555ae2a
anshumananiket <aniket.anshuman@novopay.in>
2024-01-31 18:57:45 +0530

    API mocking

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,43 @@
-const express = require('express');
-const { createServer } = require('@stoplight/prism-http-server');
+const express = require("express");
+const fs = require('fs').promises;
+const {
+  getHttpOperationsFromSpec,
+} = require("@stoplight/prism-cli/dist/operations");
+const {
+  createClientFromOperations,
+} = require("@stoplight/prism-http/dist/client");
+const { URL } = require("url");
+const axios = require('axios')
 
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const PORT = 3001;
 
 app.use(cors());
 
-const createPrismServer = async () => {
-  const prism = await createServer({});
-
-  console.log(`Prism is running at http://localhost:${prism.config.port}`);
-
-  return prism;
-};
-
-app.get('/mock', async (req, res) => {
+app.get("/", async (req, res) => {
   const apiUrl = req.query.apiUrl;
   console.log("apiUrl", apiUrl);
 
   if (!apiUrl) {
-    return res.status(400).json({ error: 'API URL is required' });
+    return res.status(400).json({ error: "API URL is required" });
   }
 
-  const prism = await createPrismServer();
+  
+});
 
-  try {
-    // Mock the response using Prism in dynamic mode
-    const response = await prism.process({
-      method: 'GET',
-      url: req.url,
-      remote: apiUrl,
-    });
-
-    res.status(response.statusCode).json(response.body);
-  } catch (error) {
-    console.error('Error mocking API:', error);
-    res.status(500).json({ error: 'Error mocking API' });
-  } finally {
-    // Close the Prism server after processing the request
-    await prism.close();
+app.get("/mock", async (req, res) => {
+  const apiUrl = req.query.apiUrl;
+  const methodType = req.method;
+  let internalApiResponse = null;
+  if("GET" === methodType){
+    internalApiResponse = await axios.get(apiUrl);
+  }else if("POST" === methodType){
+    internalApiResponse = await axios.post(apiUrl,req);
   }
+  const processedData = internalApiResponse.data;
+  await fs.writeFile('response.json', JSON.stringify(processedData, null, 2), 'utf-8');
+  res.status(200).json({ message: "Success", data: processedData });
 });
 
 app.listen(PORT, () => {
